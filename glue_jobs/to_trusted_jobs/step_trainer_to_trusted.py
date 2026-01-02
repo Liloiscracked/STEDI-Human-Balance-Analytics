1fec2accdbbdb7fb727b87f6a56b61bc5ecc26de
Liloiscracked <mr.moodyaljoudy@gmail.com>
2026-01-02 18:15:26 +0300

    addressed the issue

--- glue_jobs/to_trusted_jobs/step_trainer_to_trusted.py
+++ glue_jobs/to_trusted_jobs/step_trainer_to_trusted.py
@@ -26,18 +26,18 @@ DEFAULT_DATA_QUALITY_RULESET = """
     ]
 """
 
-# Script generated for node customer_trusted
-customer_trusted_node1767353916594 = glueContext.create_dynamic_frame.from_options(format_options={"multiLine": "false"}, connection_type="s3", format="json", connection_options={"paths": ["s3://mo-project-cutomers-trusted"], "recurse": True}, transformation_ctx="customer_trusted_node1767353916594")
+# Script generated for node customer_curated
+customer_curated_node1767353916594 = glueContext.create_dynamic_frame.from_options(format_options={"multiLine": "false"}, connection_type="s3", format="json", connection_options={"paths": ["s3://mo-project-customers-curated"], "recurse": True}, transformation_ctx="customer_curated_node1767353916594")
 
 # Script generated for node step trainer landing
 steptrainerlanding_node1767354516314 = glueContext.create_dynamic_frame.from_options(format_options={"multiLine": "false"}, connection_type="s3", format="json", connection_options={"paths": ["s3://mo-project-landing/step_trainer/landing/"], "recurse": True}, transformation_ctx="steptrainerlanding_node1767354516314")
 
 # Script generated for node SQL Query
 SqlQuery0 = '''
-select * from step_trainer_landing join customer_trusted 
-on customer_trusted.serialnumber = step_trainer_landing.serialNumber
+select * from step_trainer_landing join customer_curated 
+on customer_curated.serialnumber = step_trainer_landing.serialNumber
 '''
-SQLQuery_node1767354556237 = sparkSqlQuery(glueContext, query = SqlQuery0, mapping = {"step_trainer_landing":steptrainerlanding_node1767354516314, "customer_trusted":customer_trusted_node1767353916594}, transformation_ctx = "SQLQuery_node1767354556237")
+SQLQuery_node1767354556237 = sparkSqlQuery(glueContext, query = SqlQuery0, mapping = {"step_trainer_landing":steptrainerlanding_node1767354516314, "customer_curated":customer_curated_node1767353916594}, transformation_ctx = "SQLQuery_node1767354556237")
 
 # Script generated for node Select Fields
 SelectFields_node1767354916942 = SelectFields.apply(frame=SQLQuery_node1767354556237, paths=["sensorReadingTime", "serialNumber", "distanceFromObject"], transformation_ctx="SelectFields_node1767354916942")
